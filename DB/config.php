7f1cd61d57980c84ec944570f8b024762ce9f79d
Ngadomskiy09 <ngadomskiy09@gmail.com>
2021-05-16 01:53:27 -0700

    added db

--- DB/config.php
+++ DB/config.php
@@ -1,8 +1,27 @@
 <?php
 
+'mysql' => [
+    'driver' => 'mysql',
+    'host' => env('DB_HOST', 'remotemysql.com'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'nytupLSUX3'),
+    'username' => env('DB_USERNAME', 'nytupLSUX3'),
+    'password' => env('DB_PASSWORD', 'SJfl3Use3K'),
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'options' => [PDO::ATTR_EMULATE_PREPARES => true],
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'strict' => true,
+    'engine' => null,
+];
+
+/*
 define("DB_DSN", "mysql:host=database;dbname=mto");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "password");
+*/
 
 /*
 define("DB_DSN", "mysql:dbname=ngadomsk_grc");
